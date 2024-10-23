6278a7bb88b2a73997f892edb82e2db1cb87a35c
Gaspar3 <145287177+Gaspar3@users.noreply.github.com>
2024-10-23 11:05:55 +0200

    Create Flow-3.py

---
+++ Flow-3.py
@@ -0,0 +1,17 @@
+def Aufgabe1():
+    name1 = input("Name 1: ")
+    name2 = input("Name 2: ")
+    print(f"{name1} {name2}")
+
+def Aufgabe2():
+    text = input("Gebe einen Text: ")
+    print(len(text))
+
+def Aufgabe3():
+    text = input("Gebe einen Text: ")
+    print(text.upper())
+    print(text.lower())
+
+Aufgabe1()
+Aufgabe2()
+Aufgabe3()
